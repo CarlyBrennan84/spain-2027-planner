16f109fed2704a2cffb91d4c7002c4228031455f
CarlyBrennan84 <carly_abbott@hotmail.com>
2026-07-02 10:21:33 +1000

    Bump SW cache to v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'spain2027-v4';
+const CACHE = 'spain2027-v5';
 const ASSETS = ['./', './index.html', './app.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
